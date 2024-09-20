782138480a30d10b8e7493b2fe9b8a7d90060eb6
Ahmet Ceylan <ahmetceylan@Ahmets-MBP.home>
2024-09-20 13:58:12 +0300

    add some tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
-process.env.PORT=3000
+
+process.env.APP_PORT=3000
+process.env.APP_URL="http://localhost:3000"
+process.env.DB_HOST='localhost'
+process.env.DB_PORT=5432
+process.env.DB_USERNAME='invent_analytics_case_user'
+process.env.DB_PASSWORD='1234@Ahmet'
+process.env.DB_DATABASE='invent_analytics_db'
 process.env.NODE_ENV='development'
 
 module.exports = {
   "testEnvironment": "node",
+  "preset": "ts-jest",
   "coveragePathIgnorePatterns": [
     "/node_modules/"
   ]
---
+++ src/tests/notFound.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import application from "../app";
+
+
+describe('middlewares/notFound', () => {
+
+  afterEach(async () => {
+    await application.close();
+  });
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  
+  const getRequests = () => request(application).post('/api/invalid').send({
+    name: "sample"
+  });
+
+  it('url not found test', async () => {
+    const res = await getRequests();
+    expect(res.status).toBe(404);
+  });
+});
